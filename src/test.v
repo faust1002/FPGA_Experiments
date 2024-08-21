4b8c9f759ca0423525f29fb06a87aec058b3b0c1
Pawel <contact@pjtelecommunication.com>
2024-08-21 13:50:28 +0000

    Add a second counter and set LEDs to a product of two counters (with additional offset)

--- src/test.v
+++ src/test.v
@@ -4,17 +4,21 @@ module test(input wire clk,
             input wire rst,
             output wire [3:0] led);
 
-    reg [15:0] counter;
+    reg [15:0] primary_counter;
+    reg [15:0] secondary_counter;
 
     always @(posedge clk, posedge rst) begin
         if (rst) begin
-            counter <= 16'b0;
+            primary_counter <= 16'd37;
+            secondary_counter <= 16'd42;
         end
         else begin
-            counter <= counter + 1'b1;
+            primary_counter <= primary_counter + 1'b1;
+            secondary_counter <= secondary_counter + 1'b1;
         end
     end
 
-    assign led = counter[3:0];
+    wire [32:0] final_result = primary_counter * secondary_counter + 1'b1;
+    assign led = final_result[3:0];
 
 endmodule
--- tb/test_tb.py
+++ tb/test_tb.py
@@ -8,7 +8,18 @@ from cocotb.types import LogicArray
 
 number_of_led_bits = 4
 number_of_counter_bits = 16
-led_mask = (1 << (number_of_led_bits)) - 1
+max_led_value = 2 ** number_of_led_bits
+max_counter_value = 2 ** number_of_counter_bits
+
+primary_counter_starting_value = 37;
+secondary_counter_starting_value = 42;
+additional_offset = 1;
+
+def calculate_led_value(iteration = 0):
+    primary_counter_value = (primary_counter_starting_value + iteration) % max_counter_value
+    secondary_counter_value = (secondary_counter_starting_value + iteration) % max_counter_value
+    led_value = (primary_counter_value * secondary_counter_value + additional_offset) % max_led_value
+    return led_value
 
 async def reset_dut(reset, duration_ns):
     reset.value = 1;
@@ -17,7 +28,7 @@ async def reset_dut(reset, duration_ns):
 
 @cocotb.test()
 async def test_tb_basic_scenario(dut):
-    """Test that led is set to four LSBs of the internal 16 bit counter"""
+    """Test that led output is equal to four LSBs of a product of two internal 16-bit counters, with an additional offset added"""
 
     cocotb.log.debug("Applying reset")
 
@@ -26,7 +37,7 @@ async def test_tb_basic_scenario(dut):
     cocotb.log.debug("Reset completed")
 
     # Initially, after reset is applied, but clk is not enabled, all values should be set to 0
-    assert LogicArray(dut.led.value) == LogicArray(number_of_led_bits * '0')
+    assert dut.led.value == calculate_led_value()
 
     clock = Clock(dut.clk, 10, units="ns") #Create a 10us period clock on port clk
     cocotb.start_soon(clock.start(start_high = False))
@@ -35,12 +46,13 @@ async def test_tb_basic_scenario(dut):
 
     # Synchronise with the clock
     await RisingEdge(dut.clk)
-    for i in range(1, 2 ** number_of_counter_bits): # As soon as the first rising edge of the clock arrives, led is increased by one. Therefore we start iterating from 1, not 0
+    for idx in range(1, 2 ** number_of_counter_bits): # As soon as the first rising edge of the clock arrives, led is increased by one. Therefore we start iterating from 1, not 0
         await RisingEdge(dut.clk)
-        expected_value = i & led_mask
-        cocotb.log.debug(f"i = {i}, dut.led.value = {dut.led.value}")
-        assert dut.led.value == expected_value, f"output led value was incorrect on the {i}th cycle"
+        expected_value = calculate_led_value(idx)
+        cocotb.log.debug(f"idx = {idx}, dut.led.value = {dut.led.value}, expected_value = {expected_value}")
+        assert dut.led.value == expected_value, f"output led value was incorrect on the {idx}th cycle"
 
     # Check the final input on next clock
     await RisingEdge(dut.clk)
-    assert dut.led.value == ((2 ** number_of_counter_bits) & led_mask), f"output led was incorrect on the last cycle"
+    expected_value = calculate_led_value(2 ** number_of_counter_bits)
+    assert dut.led.value == expected_value, f"output led was incorrect on the last cycle"
