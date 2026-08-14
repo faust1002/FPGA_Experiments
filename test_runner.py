00c5bc753275e11987c5f176450b532fa8679755
faust1002 <faust1002@gmail.com>
2026-08-14 20:23:09 +0200

    Refactor test_runner so that adding new test cases will be easier in the future

--- test_runner.py
+++ test_runner.py
@@ -1,34 +1,58 @@
 #!/usr/bin/env python3
 
 import os
+from dataclasses import dataclass
 from pathlib import Path
 
 from cocotb_tools.runner import get_runner
 
 
-def test_my_design_runner():
+@dataclass
+class SimulatorDescriptor:
+    toplevel: str
+    sources: list
+    test_module: str
+    env_variables: dict
+    build_dir: str
+    waves: bool
+    verbose: bool
+
+def run_simulation(sim, simulation_descriptor : SimulatorDescriptor) -> None:
+    runner = get_runner(sim)
+    runner.build(sources      = simulation_descriptor.sources,
+                 hdl_toplevel = simulation_descriptor.toplevel,
+                 verbose      = simulation_descriptor.verbose,
+                 build_dir    = simulation_descriptor.build_dir,
+                 waves        = simulation_descriptor.waves)
+
+    runner.test(hdl_toplevel  = simulation_descriptor.toplevel,
+                test_module   = simulation_descriptor.test_module,
+                extra_env     = simulation_descriptor.env_variables,
+                waves         = simulation_descriptor.waves,
+                verbose       = simulation_descriptor.verbose)
+
+
+def test_my_design_runner() -> None:
     sim = os.getenv("SIM", "verilator")
-    build_dir = os.getenv("SIM_DIR")
+    build_dir = os.getenv("SIM_DIR", "./output/sim")
     waves = os.getenv("WAVES", "0") == "1"
 
     proj_path = Path(__file__).resolve().parent
 
-    sources = [proj_path / "rtl/top.v", proj_path / "rtl/multiplier.v"]
-    testbenches = [proj_path / "tb"]
-    env_variables = {"PYTHONPATH": testbenches}
+    simulations = [
+        SimulatorDescriptor(
+            toplevel      = "top",
+            sources       = [proj_path / "rtl/top.v", proj_path / "rtl/multiplier.v"],
+            test_module   = "test_tb",
+            env_variables = {"PYTHONPATH": [proj_path / "tb"]},
+            build_dir     = build_dir,
+            waves         = waves,
+            verbose       = True
+        )
+    ]
 
-    runner = get_runner(sim)
-    runner.build(sources = sources,
-                 hdl_toplevel = "top",
-                 verbose = True,
-                 build_dir = build_dir,
-                 waves = waves)
-
-    runner.test(hdl_toplevel = "top",
-                test_module = "test_tb",
-                extra_env = env_variables,
-                waves = waves,
-                verbose = True)
+    for simulation in simulations:
+        run_simulation(sim, simulation)
 
 if __name__ == "__main__":
     test_my_design_runner()
